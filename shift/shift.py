ecc38f7776ac65c5e43cc583349370e3e72784ff
fangliang <f304646673@gmail.com>
2024-01-16 13:01:52 +0000

    add shift

--- shift/shift.py
+++ shift/shift.py
@@ -4,11 +4,6 @@ import scipy.ndimage as ndimage
 
 source = np.array(Image.open('lena.png'))
 
-# colorDim3List = np.dsplit(source, 3)
-# red = colorDim3List[0].reshape(source.shape[0], source.shape[1])
-# green = colorDim3List[1].reshape(source.shape[0], source.shape[1])
-# blue = colorDim3List[2].reshape(source.shape[0], source.shape[1])
-
 modeList = ['reflect', 'grid-mirror', 'constant', 'grid-constant', 'nearest', 'mirror', 'grid-wrap', 'wrap']
 for mode in modeList:
     target = ndimage.shift(source, (30, 100, 0), mode=mode)
